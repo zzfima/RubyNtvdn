ba4e3791d0a0dffc1369cba9925d1be986e8814f
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-07-31 16:05:38 +0300

    rfct

---
+++ 2 dtata types.rb
@@ -0,0 +1,7 @@
+# print types
+puts "#{2.class}"
+puts "#{3.4.class}"
+puts "#{3.4e-2.class}"
+puts "#{"dod".class}"
+puts "#{10001010.class}"
+puts "#{7678676887676868765765765757556657565688.class}"
