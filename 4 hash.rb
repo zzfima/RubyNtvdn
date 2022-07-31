d3cc0fa74fa511ba8c640a48b2a1dd65febc49cc
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-07-31 18:49:42 +0300

    rfct

--- 4 hash.rb
+++ 4 hash.rb
@@ -2,3 +2,10 @@ hash1 = { "one" => 1, "five" => 5 }
 puts hash1.values
 puts hash1.keys
 puts hash1["five"]
+
+hash2 = { "work" => "kla" }
+puts hash1.merge(hash2)
+puts hash1.invert
+hash3 = { name: 'Vasia', age: 33 }
+puts hash3
+puts hash3[:age]
