65b0d28f0a45ec2fc43d3c357356e7bd2e775fa6
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-08-02 09:02:31 +0300

    rfct

--- 8 classes.rb
+++ 8 classes.rb
@@ -1,14 +1,21 @@
 class Animal
-  def get_weight
-    55
+  #ctor
+  def initialize(w)
+    @weight = w
   end
 
+  #object method
+  def get_weight
+    @weight
+  end
+
+  #class method
   def Animal.class_name
     "Animal"
   end
 end
 
-a1 = Animal.new
+a1 = Animal.new(11)
 puts a1.get_weight
 
 puts Animal.class_name
